95ede0055511d7635c85552b01a3905af8ea9690
basan nofal <nofalbasan@gmail.com>
2023-05-08 10:06:40 +0530

    changes in develop branch

--- MERN Stack/Server/Allgetdata.js
+++
@@ -1,9 +0,0 @@
-app.get('/getdata', async (req, res) => {
-    try {
-        const userdata = await getschema.find();
-        res.status(201).json(userdata);
-    } catch (error) {
-        res.send(error)
-    }
-
-})
